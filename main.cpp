7f5764e87615b16744f4c0f40ae485d8e3790ee9
jesus-tl <jesus.torres21@upr.edu>
2024-09-09 14:37:15 +0000

    Purpose

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+// Jesus M Torres Luquis, jesus-tl, 801 20 3215
+#include<iostream>
+using namespace std;
 
+int main(){
+
+    //Display purpose of program
+    cout << "This program will calculate the Gross and Net Box Office Profit
+    and the Distributer's Profit of a movie playing in the theater."
+
+
+
+return 0;
+
+}
